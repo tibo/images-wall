154f3ea7beb0460cc4f924f994396b5b0d409de3
Thibaut LE LEVIER <thibaut@lelevier.fr>
2014-11-18 16:45:13 +0100

    import project

---
+++ index.js
@@ -0,0 +1,23 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/new-image', function(req, res){
+
+	img = req.query['img'];
+
+	if (img)
+	{
+		io.emit('new image', img);
+	}
+
+	res.send();
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
